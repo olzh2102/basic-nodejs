fd72ec03188e7337e58281466f3395ed51c6cd23
Olzhas Kurikov <okur@hellofresh.com>
2021-11-17 18:07:52 +0100

    test: prepare todo unit tests for parser fn

---
+++ my_ciphering_cli/parser.test.js
@@ -0,0 +1,9 @@
+const parse = require('./parser')
+
+describe('Parser', () => {
+    it.todo('returns expected inputPath, outputPath and cipher pattern')
+    it.todo('throws Error if any argument is appeared more than once')
+    it.todo('throws Error if no config flag provided')
+    it.todo('throws Error if user types short and long variants of same flag')
+    it.todo('throws Error if no value provided after flag')
+})
