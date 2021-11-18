6b66486961624011cddb465e2790f112c312c92f
Olzhas Kurikov <okur@hellofresh.com>
2021-11-18 23:18:18 +0100

    test: parses correctly and no argument after flag

--- my_ciphering_cli/parser.test.js
+++ my_ciphering_cli/parser.test.js
@@ -1,16 +1,29 @@
 const parse = require('./parser')
-const { RepeatedArgumentError } = require('./custom-errors')
+const { RepeatedArgumentError, NoValueFoundAfterFlagError } = require('./custom-errors')
 
 describe('Parser', () => {
+    it('returns expected inputPath, outputPath and cipher pattern', () => {
+        const args = ['-c', 'C1-R1-C0-R0-A', '--input', './input.txt', '-o', './output.txt']
+        const result = parse(args)
+        expect(result).toEqual({
+            input: './input.txt',
+            output: './output.txt',
+            pattern: 'C1-R1-C0-R0-A'
+        })
+    })
+
     it('throws Error if any argument is appeared more than once', () => {
         const args = ['-c', 'C1', '-c']
         const result = () => parse(args)
         expect(result).toThrowError(RepeatedArgumentError)
     })
 
-    it.todo('returns expected inputPath, outputPath and cipher pattern')
-    it.todo('throws Error if any argument is appeared more than once')
+    it('throws Error if no value provided after flag', () => {
+        const args = ['-c', 'C1', '-i']
+        const result = () => parse(args)
+        expect(result).toThrowError(NoValueFoundAfterFlagError)
+    })
+
     it.todo('throws Error if no config flag provided')
     it.todo('throws Error if user types short and long variants of same flag')
-    it.todo('throws Error if no value provided after flag')
 })
