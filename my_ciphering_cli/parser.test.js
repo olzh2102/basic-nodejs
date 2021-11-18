3a34abf3489add99d0bfd485744a25c741072608
Olzhas Kurikov <okur@hellofresh.com>
2021-11-18 12:13:24 +0100

    test: parser - if any argument is appeared more than once

--- my_ciphering_cli/parser.test.js
+++ my_ciphering_cli/parser.test.js
@@ -1,6 +1,13 @@
 const parse = require('./parser')
+const { RepeatedArgumentError } = require('./custom-errors')
 
 describe('Parser', () => {
+    it('throws Error if any argument is appeared more than once', () => {
+        const args = ['-c', 'C1', '-c']
+        const result = () => parse(args)
+        expect(result).toThrowError(RepeatedArgumentError)
+    })
+
     it.todo('returns expected inputPath, outputPath and cipher pattern')
     it.todo('throws Error if any argument is appeared more than once')
     it.todo('throws Error if no config flag provided')
